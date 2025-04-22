a933488b373ceb079d603c7c2b8d884a4081c928
ziasvannes <ziasvannes@ziasserver>
2025-04-22 14:47:54 +0200

    Add the database provider and add landing page.

--- api/index.py
+++ api/index.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
+from supabase import create_client, Client
+
 app = Flask(__name__)
 
-@app.route("/api/python")
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+supabase: Client = create_client(url, key)
+
+@app.route("/api/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
