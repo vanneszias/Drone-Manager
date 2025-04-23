6618620338f39ffe001bc2d4fa9859ed1c0703de
ziasvannes <ziasvannes@ziasserver>
2025-04-23 14:01:31 +0200

    Some fixes in the api and hooks.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -17,8 +17,8 @@ export default function DashboardLayout({
         <aside className="w-64 border-r p-4 hidden md:block">
           <nav>
             {/* Navigation Links */}
-            <a href='/dashboard/zones' className="block py-2 px-4 hover:bg-muted rounded">Zones</a>
             <a href="/dashboard/event" className="block py-2 px-4 hover:bg-muted rounded">Events</a>
+            <a href='/dashboard/zones' className="block py-2 px-4 hover:bg-muted rounded">Zones</a>
             <a href="/dashboard/drones" className="block py-2 px-4 hover:bg-muted rounded">Drones</a>
           </nav>
         </aside>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function DashboardPage() {
-  return <div>Dashboard</div>;
+  redirect("/dashboard/event");
 }
